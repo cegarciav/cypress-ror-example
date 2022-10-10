d945989485e83745449df05c1fcb94627ec11189
cegarciav <cegarcia2@uc.cl>
2022-10-09 21:18:09 -0300

    Add check_credentials method

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def check_credentials
+    session_params = params.permit(
+      :username,
+      :password,
+      :authenticity_token,
+      :commit
+    )
+    @email_user = User.find_by(email: session_params[:username])
+    if @email_user
+      @user = @email_user
+    else
+      @user = User.find_by(username: session_params[:username])
+    end
+    
+    if @user
+      if @user.password == session_params[:password]
+        session[:username] = @user.username
+        redirect_to '/'
+      else
+        flash[:notice] = 'Invalid credentials!'
+        redirect_to '/login'
+      end
+    else
+      flash[:notice] = 'User not found!'
+      redirect_to '/login'
+    end
+  end
+end
