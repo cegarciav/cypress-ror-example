a8ed69883f357cd9a8fe0684f7eca1c59a51b66a
cegarciav <cegarcia2@uc.cl>
2022-10-10 03:05:41 -0300

    Add create method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,46 @@ class UsersController < ApplicationController
     end
   end
 
+  def create
+    new_user_params = params.permit(
+      :email,
+      :name,
+      :username,
+      :password,
+      :password2,
+      :authenticity_token,
+      :commit
+    )
+
+    if new_user_params[:password] != new_user_params[:password2]
+      flash[:notice] = 'Passwords don\'t match'
+      redirect_to '/signup'
+    elsif new_user_params[:password].length < 8
+      flash[:notice] = 'Password must be at least 8 characters long'
+      redirect_to '/signup'
+    elsif User.find_by(email: new_user_params[:email])
+      flash[:notice] = 'Email already registered'
+      redirect_to '/signup'
+    elsif User.find_by(username: new_user_params[:username])
+      flash[:notice] = 'Username already registered'
+      redirect_to '/signup'
+    else
+      begin
+        @user = User.create!(
+          email: new_user_params[:email],
+          name: new_user_params[:name],
+          username: new_user_params[:username],
+          password: new_user_params[:password]
+        )
+        session[:username] = @user.username
+        redirect_to '/'
+      rescue
+        flash[:notice] = 'Invalid email'
+        redirect_to '/signup'
+      end
+    end
+  end
+
   def login
     if session[:username] && User.find_by(username: session[:username])
       redirect_to '/'
