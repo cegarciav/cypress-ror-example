a04dac10dd15fa85c86fa354e6dc78f6616aa06b
cegarciav <cegarcia2@uc.cl>
2022-10-10 20:40:36 -0300

    Set Articles index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root 'articles#index'
+
   get '/login', to: 'users#login'
   get '/signup', to: 'users#signup'
   post '/users', to: 'users#create'
