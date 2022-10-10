162564c9c0c782ef19940cab7738c2abfeef3877
cegarciav <cegarcia2@uc.cl>
2022-10-09 21:13:17 -0300

    Add routes to login and check credentials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/login', to: 'users#login'
+  post '/users/check-credentials', to: 'users#check_credentials'
 end
