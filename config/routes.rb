d798926679e6371ff22c419718267d0dacb842df
cegarciav <cegarcia2@uc.cl>
2022-10-10 03:07:15 -0300

    Add signup and user-create routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get '/login', to: 'users#login'
+  get '/signup', to: 'users#signup'
+  post '/users', to: 'users#create'
   post '/users/check-credentials', to: 'users#check_credentials'
 end
