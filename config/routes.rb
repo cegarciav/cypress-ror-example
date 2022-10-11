39ca1812464527bc7307a7065403ff6443ff310a
cegarciav <cegarcia2@uc.cl>
2022-10-11 01:55:15 -0300

    Add article routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
 
   root 'articles#index'
 
+  # Users
   get '/login', to: 'users#login'
   get '/signup', to: 'users#signup'
   post '/users', to: 'users#create'
   post '/users/check-credentials', to: 'users#check_credentials'
+
+  # Articles
+  get '/articles/new', to: 'articles#new'
+  post '/articles', to: 'articles#create'
 end
