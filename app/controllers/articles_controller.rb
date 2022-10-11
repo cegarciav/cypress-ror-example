77fd8f1fa1ad9a90716fa0eb7abc9e652949a4c1
cegarciav <cegarcia2@uc.cl>
2022-10-11 01:54:48 -0300

    Add method to create an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,35 @@
 class ArticlesController < ApplicationController
+  def create
+    if !(session[:username] && User.find_by(username: session[:username]))
+      redirect_to '/'
+    end
+
+    new_article_params = params.permit(
+      :title,
+      :description,
+      :image_url,
+      :authenticity_token,
+      :commit
+    )
+
+    if new_article_params[:title].length < 1
+      flash[:notice] = 'Title must be at least 1 character long'
+      redirect_to '/articles/new'
+    else
+      begin
+        @article = Article.create!(
+          title: new_article_params[:title],
+          description: new_article_params[:description],
+          image_url: new_article_params[:image_url]
+        )
+        redirect_to '/'
+      rescue
+        flash[:notice] = 'Something went wrong. Check the content and try again'
+        redirect_to '/articles/new'
+      end
+    end
+  end
+
   def index
     @articles = Article.all
   end
