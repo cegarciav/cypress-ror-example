db545c816364eef848a3f4ff1d02e90c7accc56c
cegarciav <cegarcia2@uc.cl>
2022-10-31 18:27:43 -0300

    Add seeds for User and Article

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+    Article.destroy_all
+    User.destroy_all
+
+    User.create!({
+        email: "nelson.haha@gmail.com",
+        name: "Nelson Muntz",
+        password: "colección-de-estampas-haha",
+        username: "nmuntz"
+    })
+    Article.create!([{
+        title: "Festival de cine",
+        description: "Frame de mi cortometraje en el festival de Sundance",
+        image_url: "https://i.ytimg.com/vi/qt65_GTEVBI/hqdefault.jpg"
+    }, {
+        title: "¿Será posible?",
+        description: "",
+        image_url: "https://preview.redd.it/yt1sqo8q1s911.jpg?width=640&crop=smart&auto=webp&s=47eefcc90981bfcdc88356098271821f198646bc"
+    }])
