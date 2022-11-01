a3661ef849afbd80e3a2859f599d18c5cb15af56
cegarciav <cegarcia2@uc.cl>
2022-10-31 21:16:35 -0300

    Add test to check if a user can create a new article after log-in

--- cypress/e2e/registered-user.cy.js
+++ cypress/e2e/registered-user.cy.js
@@ -46,4 +46,46 @@ describe('Registered user visiting the site', () => {
     );
     cy.contains('a', 'New Article');
   });
+
+  it('should allow a logged-in user to create a new article', () => {
+    cy.visit('/');
+
+    // Go to /login view
+    cy.contains('a', 'Log In').click();
+    cy.url().should('include', '/login');
+
+    // Login form
+    cy.get('input[id="username"]').type('nelson.haha@gmail.com');
+    cy.get('input[id="password"]').type('colección-de-estampas-haha');
+
+    cy.contains('input', 'Log In').click();
+
+    // Redirection to homepage
+    cy.url().should(
+      'eq',
+      `${Cypress.config("baseUrl")}/`,
+    );
+
+    // Create a new article
+    const articleTitle = 'New test from Cypress';
+    const articleDescription = 'If you see this, we were able to create a new article';
+    const articleImage = 'https://m.media-amazon.com/images/I/418r9PmmoSL._SL500_.jpg';
+    cy.contains('a', 'New Article').click();
+    cy.get('input[id="title"]').type(articleTitle);
+    cy.get('input[name="description"]').type(articleDescription);
+    cy.get('#image_url').type(articleImage);
+
+    cy.get('form').submit()
+
+    // Redirection to homepage
+    cy.url().should(
+      'eq',
+      `${Cypress.config("baseUrl")}/`,
+    );
+    cy.contains('h1', articleTitle);
+    cy.contains('p', articleDescription);
+    cy.get('img')
+      .should('have.attr', 'src')
+      .should('eq',articleImage);
+  });
 });
