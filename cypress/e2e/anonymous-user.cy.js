f828329f612d8f7ecea5132ebde36da1b6582e31
cegarciav <cegarcia2@uc.cl>
2022-10-31 21:09:46 -0300

    Add test to check if an anonymous user can access the homepage

--- cypress/e2e/anonymous-user.cy.js
+++ cypress/e2e/anonymous-user.cy.js
@@ -5,7 +5,18 @@ describe('Anonoymous user visiting the site', () => {
       .should('eq', 0);
   });
 
-  it('passes', () => {
+  it('should be able to access the homepage', () => {
     cy.visit('/');
+
+    // Navbar display right links
+    cy.contains('a', 'Log In');
+    cy.contains('a', 'Sign Up');
+    cy.contains('a', 'Home');
+
+    // Displays the list of articles
+    cy.contains('h1', 'List of articles');
+    cy.contains('h1', '¿Será posible?');
+    cy.contains('h1', 'Festival de cine');
+    cy.contains('p', 'Frame de mi cortometraje en el festival de Sundance');
   });
 });
