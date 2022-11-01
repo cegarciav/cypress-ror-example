17060b96ef0d7a5cf547b038d7b42eadb1f613f6
cegarciav <cegarcia2@uc.cl>
2022-10-31 21:14:10 -0300

    Add test to check if a user can login with their username

--- cypress/e2e/registered-user.cy.js
+++ cypress/e2e/registered-user.cy.js
@@ -4,4 +4,25 @@ describe('Registered user visiting the site', () => {
       .its('code')
       .should('eq', 0);
   });
+
+  it('should be able to log in with their username', () => {
+    cy.visit('/');
+
+    // Go to /login view
+    cy.contains('a', 'Log In').click();
+    cy.url().should('include', '/login');
+
+    // Login form
+    cy.get('input[id="username"]').type('nmuntz');
+    cy.get('input[id="password"]').type('colección-de-estampas-haha');
+
+    cy.contains('input', 'Log In').click();
+
+    // Redirection to homepage
+    cy.url().should(
+      'eq',
+      `${Cypress.config("baseUrl")}/`,
+    );
+    cy.contains('a', 'New Article');
+  });
 });
